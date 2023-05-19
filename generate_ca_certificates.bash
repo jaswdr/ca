6a3a8445dc2aaa00709209df44f40f9fee4853ed
Jonathan Schweder <jonathanschweder@amazon.com>
2023-05-19 13:45:00 +0100

    Better comments

--- generate_ca_certificates.bash
+++ generate_ca_certificates.bash
@@ -2,6 +2,11 @@
 
 rm ca.key ca.csr ca.crt
 
+# Generate the private key
 openssl ecparam -out ca.key -name prime256v1 -genkey
+
+# Create the CSR
 openssl req -new -sha256 -key ca.key -out ca.csr
+
+# Generate the CA certificate
 openssl x509 -req -sha256 -days 365 -in ca.csr -signkey ca.key -out ca.crt
